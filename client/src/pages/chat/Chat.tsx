df953fbd3df32df8e98680db0382bd71d78c7534
tomcorey26 <tomcorey26@my.uri.edu>
2022-12-26 20:57:37 -0500

    Move ui into own dir

--- client/src/pages/chat/Chat.tsx
+++ client/src/pages/chat/Chat.tsx
@@ -5,11 +5,12 @@ export const Chat = () => {
     <div className="grid h-screen grid-cols-3 grid-rows-2">
       <div className="col-span-2">
         {/* USER */}
-        <ChatVideo />
+        {/* <ChatVideo /> */}
       </div>
       <div className="col-span-1 row-span-2 "></div>
       <div className="col-span-2">
-        <ChatVideo />
+        {/* FRIEND */}
+        {/* <ChatVideo /> */}
       </div>
     </div>
   )
